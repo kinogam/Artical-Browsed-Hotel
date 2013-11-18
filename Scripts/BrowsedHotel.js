0e5ae469f2d71957e6c8832447bcbadc63a5a2d6
kinogam <kinogam@gmail.com>
2013-11-18 11:05:08 +0800

    update

---
+++ Scripts/BrowsedHotel.js
@@ -0,0 +1,58 @@
+﻿/// <reference path="require.js" />
+
+define([
+    "kino.razor",
+    "browserStore",
+    "exports"
+],
+    function (razor, browserStore, exports) {
+        var hotelList,
+            hasInit = false,
+            storeName = "browsed_hotel";
+
+        exports.save = function (item) {
+            ///<summary>
+            ///保存浏览过的酒店数据
+            ///</summary>
+
+            if (!hasInit) {
+                expots.init();
+            }
+
+            if (!isExist(item)) {
+                hotelList.push(item);
+            }
+
+            updateStore();
+        };
+
+        exports.getDataCount = function () {
+            return hotelList.length;
+        };
+
+        exports.init = function () {
+            ///<summary>
+            ///初始化
+            ///</summary>
+
+            hotelList = [];
+            var storeValue = browserStore.getItem(storeName);
+            if (storeValue != null) {
+                hotelList = JSON.parse(storeValue);
+            }
+            hasInit = true;
+        };
+
+        function updateStore() {
+            browserStore.setItem(storeName, JSON.stringify(hotelList));
+        }
+
+        function isExist(item) {
+            for (var i = 0, len = hotelList.length; i < len; i++) {
+                if (hotelList[i].name === item.name) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    });
---
+++ Scripts/test/BrowsedHotel-test.js
@@ -0,0 +1,46 @@
+﻿/// <reference path="qunit/qunit.js" />
+require.config({
+    baseUrl: './../',
+    paths: {
+        "kino.razor": "https://raw.github.com/kinogam/kino.razor/master/lib/kino.razor",
+        "browserStore": "https://raw.github.com/kinogam/itour/master/lib/util/itour.util.localStorage"
+    }
+});
+
+
+var stubBrowseStore = {};
+
+define("browserStore", function () {
+    return { getItem: function (itemName) { return stubBrowseStore[itemName]; }, setItem: function (itemName, itemValue) { stubBrowseStore[itemName] = itemValue; } }
+});
+
+require(["BrowsedHotel"], function (BrowsedHotel) {
+
+    module("我浏览过的酒店", {
+        setup: function () {
+            stubBrowseStore = {};
+            BrowsedHotel.init();
+        }
+    })
+
+    test("保存浏览过的酒店数据", function () {
+        equal(BrowsedHotel.getDataCount(), 0);
+        BrowsedHotel.save({ name: '广州有色金属酒店', price: 234, url: 'http://hoteltest.clt198.com' });
+        equal(BrowsedHotel.getDataCount(), 1);
+    });
+
+    test("浏览同一家酒店不会增加浏览记录", function () {
+        BrowsedHotel.save({ name: '广州有色金属酒店', price: 234, url: 'http://hoteltest.clt198.com' });
+        equal(BrowsedHotel.getDataCount(), 1);
+        BrowsedHotel.save({ name: '广州有色金属酒店', price: 234, url: 'http://hoteltest.clt198.com' });
+        equal(BrowsedHotel.getDataCount(), 1);
+    });
+
+    test("浏览同一家酒店会更新相应的酒店数据", function () {
+        BrowsedHotel.save({ name: '广州有色金属酒店', price: 234, url: 'http://hoteltest.clt198.com' });
+
+        BrowsedHotel.save({ name: '广州有色金属酒店', price: 345, url: 'http://hoteltest.clt198.com' });
+        equal(JSON.parse(stubBrowseStore["browsed_hotel"])[0].price, 345);
+    });
+
+});
